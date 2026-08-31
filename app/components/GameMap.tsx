9e4f1750bb04da85191718656c4378474c0cebac
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-08-31 12:00:58 +0530

    feat:added alot of improvements, clues, and animations

--- app/components/GameMap.tsx
+++ app/components/GameMap.tsx
@@ -1,41 +1,48 @@
 "use client";
 
 import { LatLng, LatLngBounds } from "leaflet";
-import { CircleMarker, MapContainer, Marker, Polyline, TileLayer, useMapEvents } from "react-leaflet";
+import { CircleMarker, MapContainer, Marker, Polyline, TileLayer, Tooltip, useMapEvents } from "react-leaflet";
+import { Coordinates } from '../game/types';
 type GameMapProps = {
     onLocationSelect : (location: LatLng)=>void;
-    selectedLocation : LatLng | null;
-    actualLocation : LatLng | null;
+    selectedLocation : Coordinates | null;
+    actualLocation : Coordinates | null;
+    resultShown : boolean;
+    distance : number | null;
+
 }
 
-function MapClickHandler({onLocationSelect,} : {onLocationSelect : (location : LatLng)=>void} ){
+function MapClickHandler({onLocationSelect, resultShown} : {onLocationSelect : (location : LatLng)=>void; resultShown : boolean} ){
     useMapEvents({
         click(event){
+           if(resultShown) return;
             onLocationSelect(event.latlng);
         },
     });
     return null;
 }
 
-export default function GameMap({onLocationSelect, selectedLocation, actualLocation} : GameMapProps){
+export default function GameMap({onLocationSelect, selectedLocation, actualLocation, resultShown, distance} : GameMapProps){
     const worldBounds = new LatLngBounds(
-        [-85, -180],
+        [-60, -180],
         [85, 180]
     )
     return(
         <MapContainer
         center={[20,0]}
-        zoom={2}
-        minZoom={2}
-        maxZoom={18}
+        zoom={8}
+        minZoom={3}
+        maxZoom={25}
         maxBounds={worldBounds}
+        worldCopyJump={false}
         maxBoundsViscosity={1.0}
         className="h-full w-full">
             <TileLayer 
+            noWrap={true}
             attribution="&copy; OpenStreetMap contributors"
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <MapClickHandler onLocationSelect={onLocationSelect} />
+            <MapClickHandler resultShown={resultShown} onLocationSelect={onLocationSelect} />
             {selectedLocation && (
                 <CircleMarker center={selectedLocation} radius={8} pathOptions={{
                     color : "white",
@@ -54,14 +61,17 @@ export default function GameMap({onLocationSelect, selectedLocation, actualLocat
                 }}/>
             )}
             {
-                selectedLocation && actualLocation && (
+                selectedLocation && actualLocation && distance !== null && (
                     <Polyline
                         positions={[selectedLocation, actualLocation]}
                         pathOptions={{
                             color : "black",
                             weight : 3,
-                        }}
-                    />
+                        }}>
+                            <Tooltip permanent>
+                                {distance.toFixed(1)}km
+                            </Tooltip>
+                        </Polyline>
                 )
             }
         </MapContainer>
--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,12 @@ import { LatLng } from "leaflet";
 import { locations } from "./game/locations";
 import FinalScore from "./components/FinalScore";
 import Image from "next/image";
-import { Dot } from "lucide-react";
+import { ArrowUp, Dot, Minus, Plus } from "lucide-react";
+import { AnimatePresence, motion } from "framer-motion";
 
 const GameMap = dynamic(
-  ()=> import("./components/GameMap"),
-  {ssr : false}
+  () => import("./components/GameMap"),
+  { ssr: false }
 )
 
 function calculateDistance(
@@ -30,7 +31,7 @@ function calculateDistance(
 }
 
 export default function Home() {
-  const [selectedLocation, setSelectedLocation] = useState<LatLng | null>(null);
+  const [selectedLocation, setSelectedLocation] = useState<{ lat: number; lng: number; } | null>(null);
   const [currentLocation, setCurrentLocation] = useState(
     locations[0]
   );
@@ -40,7 +41,7 @@ export default function Home() {
   const [result, setResult] = useState<{ distance: number; points: number; } | null>(null);
   const [usedLocations, setUsedLocations] = useState<number[]>([0]);
   const nextRound = () => {
-    if(round === 5){
+    if (round === 5) {
       setGameOver(true);
       return;
     }
@@ -49,44 +50,55 @@ export default function Home() {
       .filter(({ index }) => !usedLocations.includes(index));
     const randomLocation = availableLocations[Math.floor(Math.random() * availableLocations.length)];
     setCurrentLocation(randomLocation.location);
-    setUsedLocations((current)=>[
+    setUsedLocations((current) => [
       ...current,
       randomLocation.index,
     ]);
-    setRound((current)=> current + 1);
+    setRound((current) => current + 1);
     setSelectedLocation(null);
     setRevealClues(1)
     setResult(null);
+    setImageScale(1);
   }
   const [revealClues, setRevealClues] = useState(1);
+  const [imageScale, setImageScale] = useState(1);
   return (
     <main className="w-screen h-screen overflow-hidden">
-      <GameMap onLocationSelect={setSelectedLocation} selectedLocation={selectedLocation} actualLocation={result ? new LatLng(
-        currentLocation.latitude,
-        currentLocation.longitude
-      ) : null} />
-      {selectedLocation && (
-        <button
-          disabled={!!result}
-          onClick={() => {
-            const distance = calculateDistance(
-              selectedLocation.lat,
-              selectedLocation.lng,
-              currentLocation.latitude,
-              currentLocation.longitude,
-            );
-            const points = Math.max(
-              0,
-              Math.round(5000 * Math.exp(-distance / 2000))
-            )
-            setResult({
-              distance,
-              points,
-            })
-            setScore((current) => current + points);
-          }} className="absolute bottom-8 left-1/2 -translate-x-1/2 z-[1000] rounded-xl bg-black px-8 py-4 text-lg font-semibold text-white shadow-xl hover:scale-105 transition">
-          MAKE GUESS
-        </button>
+      <GameMap distance={result?.distance ?? null} onLocationSelect={setSelectedLocation} selectedLocation={selectedLocation} actualLocation={result ? { lat: currentLocation.latitude, lng: currentLocation.longitude } : null}
+        resultShown={!!result} />
+      {selectedLocation && !result && (
+        <div className="absolute bottom-8 left-1/2 -translate-x-1/2 z-[1000] rounded-xl bg-black px-8 py-4 text-lg font-semibold text-white shadow-xl hover:scale-105 transition">
+
+          <p className="text-xs uppercase tracking-widest text-zinc-500">
+            Your Guess
+          </p>
+          <p className="mt-1 text-sm text-zinc-300">
+            {selectedLocation.lat.toFixed(2)}, {" "}
+            {selectedLocation.lng.toFixed(2)}
+          </p>
+          <button
+            className="mt-3 rounded-xl bg-white px-6 py-3 font-semibold text-black transition hover:scale-105"
+            disabled={!!result}
+            onClick={() => {
+              const distance = calculateDistance(
+                selectedLocation.lat,
+                selectedLocation.lng,
+                currentLocation.latitude,
+                currentLocation.longitude,
+              );
+              const points = Math.max(
+                0,
+                Math.round(5000 * Math.exp(-distance / 2000))
+              )
+              setResult({
+                distance,
+                points,
+              })
+              setScore((current) => current + points);
+            }}>
+            MAKE GUESS
+          </button>
+        </div>
       )}
       {result && (
         <div className="absolute top-6 left-1/2 -translate-x-1/2 z-[1000] rounded-xl bg-black px-6 py-4 text-center text-white shadow-xl">
@@ -102,42 +114,80 @@ export default function Home() {
           <button
             onClick={nextRound}
             className="mt-4 rounded-lg cursor-pointer bg-white px-5 py-2 font-semibold text-black hover:scale-105 transition"
-          >NEXT ROUND
+          >
+            {round === 5 ? "SEE RESULTS" : "NEXT ROUND"}
           </button>
         </div>
       )}
       <div className="absolute top-5 right-10 z-[1000] rounded-lg bg-black px-4 py-2 text-white">
         Round {round}/5
       </div>
-      <div className="absolute top-5 left-10 z-[1000] rounded-lg bg-black text-white py-2 px-4 ">
+      <div className="absolute top-5 left-12 z-[1000] rounded-lg bg-black text-white py-2 px-4 ">
         Score : {score.toLocaleString()}
       </div>
       {gameOver && (
-        <FinalScore score={score} onRestart={()=>{
+        <FinalScore score={score} onRestart={() => {
           window.location.reload();
         }} />
       )}
-      <div className="absolute top-6 left-6 z-[1000] w-[450px] overflow-hidden rounded-2xl bg-black shadow-2xl">
-        <Image src={currentLocation.image} alt="location" width={450} height={300} className="h-[300px] w-full object-cover" />
-            <div className="p-4">
-        <p className="text-xs uppercase tracking-widest text-zinc-500">Clues</p>
-        <div className="mt-3 space-y-2">
-          {currentLocation.clues.map((clue, index)=>(
-            <div  key={index}>
-              {index < revealClues ? (
-            <p className="text-sm flex text-white">
-             <span><Dot /> </span> 
-             <span>{clue}</span>
-            </p>
-              ) : (
-                <button className="text-sm text-zinc-500 hover:text-white transition-all" onClick={()=>setRevealClues((current)=> current + 1)}>
-                  Reveal clue #{index + 1}
-                </button>
-              )}
-            </div>
-          ))}
+      <div className="absolute bottom-5 right-2 z-[1000] w-[450px] overflow-hidden rounded-2xl bg-black shadow-2xl">
+        <div className="relative w-full overflow-hidden h-[250px]">
+          <div className="absolute top-5 flex left-1/2 z-10 items-center gap-2 -translate-x-1/2 rounded-full bg-black/80 p-2 text-xs font-medium text-white backdrop-blur-sm">
+            N <ArrowUp size={12} />
+          </div>
+          <div className="absolute bottom-3 text-white left-3 z-20  rounded-lg  bg-black/80 px-3 py-2 text-xs uppercase tracking-widest backdrop-blur-sm">
+            Explore
+          </div>
+          <AnimatePresence mode="wait">
+            <motion.div
+            key={currentLocation.id}
+            initial={{opacity : 0, scale : 1.05}}
+            animate={{opacity : 1, scale : 1}}
+            exit={{opacity : 0, scale : 0.98}}
+            transition={{duration : 0.4}}
+            className="w-full h-full">
+          <Image
+            src={currentLocation.image}
+            alt="location"
+            width={450}
+            height={300}
+            className="h-full w-full object-cover"
+            style={{ transform: `scale(${imageScale})` }}
+          />
+          </motion.div>
+          </AnimatePresence>
         </div>
-      </div>
+        <div className="absolute bottom-3 right-3 flex gap-2 z-20">
+          <button className="rounded-lg bg-black/80 px-3 py-2 text-white" onClick={() => setImageScale((current) => Math.min(current + 0.25, 2.5))}>
+            <Plus />
+          </button>
+          <button className="rounded-lg bg-black/80 px-3 py-2 text-white " onClick={() => setImageScale((current) => Math.max(current - 0.25, 1))}>
+            <Minus />
+          </button>
+        </div>
+        <div className="p-4 z-[100000]">
+          <p className="text-xs uppercase tracking-widest text-zinc-500">Clues</p>
+          <div className="mt-3 space-y-2">
+            {currentLocation.clues.map((clue, index) => (
+              <div key={index}>
+                {index < revealClues ? (
+                  <p className="text-sm flex text-white">
+                    <span><Dot /> </span>
+                    <span>{clue}</span>
+                  </p>
+                ) : index === revealClues ? (
+                  <button className="text-sm text-zinc-500 hover:text-white transition-all" onClick={() => {
+                    const cost = index === 1 ? 250 : 500;
+                    setScore((current) => Math.max(0, current - cost));
+                    setRevealClues((current) => current + 1);
+                  }}>
+                    Reveal clue #{index + 1} (-{index === 1 ? 250 : 500})
+                  </button>
+                ) : null}
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
 
     </main>
